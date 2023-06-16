f9d59db9505a83b656379faf12538f1bbec81fee
KCY-G <1094ap@kaist.ac.kr>
2023-06-16 17:31:47 +0900

    restructure

--- pages/api/signupHandler.js
+++
@@ -1,7 +0,0 @@
-//      FINISHED: X
-
-//      FROM: start/signup.js
-//      DATA: username, email, password, profileimage, contact
-//      TRANSFER: formdata?
-//      FUNCTION: 전달받은 변수를 통해 만든 새로운 계정정보를 DB에 추가, 성공하면 index로 리디렉션
-//      TO: index.js
