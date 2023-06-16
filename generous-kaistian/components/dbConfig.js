2c9d4b71220d11c3de324b837b34e9d05a144c56
KCY-G <1094ap@kaist.ac.kr>
2023-06-16 17:24:44 +0900

    DB server login

--- generous-kaistian/components/dbConfig.js
+++ generous-kaistian/components/dbConfig.js
@@ -5,7 +5,7 @@ const client = new Client({
   user: 'postgres',
   host: 'generous-kaistian-testdb.c2wnjmcl1kwe.ap-northeast-2.rds.amazonaws.com',
   database: 'testdb',
-  password: ':0oXbKZqW0Uo2eYW3lXVH',
+  password: '0oXbKZqW0Uo2eYW3lXVH',
   port: 5432, // or your PostgreSQL port
 });
 
